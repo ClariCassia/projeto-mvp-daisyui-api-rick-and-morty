cf8e3fd3e8846c3f4a4efee6adc632736e4ae4cd
Clarissa <clari.cassia@gmail.com>
2026-03-03 11:14:58 -0300

    feat: atualiza NavBar para incluir logo e adiciona imagens decorativas nas laterais do layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <a class="btn btn-ghost text-xl">The Rick and Morty </a>
+  <div class="navbar bg-base-100 shadow-sm justify-center fixed top-0 left-0 w-full z-50">
+    <div class="w-20 p-3">
+      <img src="/public/images/logo-rick-morty.jpg" alt="Logo Rick and Morty" />
+    </div>
+    <h1 class="btn btn-ghost text-xl">The Rick and Morty </h1>
   </div>
 </template>
--- src/layouts/LayoutDefault.vue
+++ src/layouts/LayoutDefault.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
-    <NavBar />
-    <RouterView />
+  <NavBar />
+  
+  <div class="flex w-full min-h-screen pt-20 bg-base-200">
+    
+    <aside class="hidden xl:block w-1/5 relative">
+      <div class="sticky top-20 h-[calc(100vh-5rem)] flex items-center justify-center p-4">
+        <img src="/public/images/Desenhe-Rick-de-Rick-e-Morty.png" class="max-w-full object-contain opacity-50" />
+      </div>
+    </aside>
+    
+    <main class="flex-1 p-4 md:p-8">
+      <RouterView />
+    </main>
+    
+    <aside class="hidden xl:block w-1/5 relative">
+      <div class="sticky top-20 h-[calc(100vh-5rem)] flex items-center justify-center p-4">
+        <img src="/public/images/Desenhe-2-Rick-de-Rick-e-Morty.png" class="max-w-full object-contain opacity-50 scale-x-[-1]" />
+      </div>
+    </aside>
+    
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+</script>
